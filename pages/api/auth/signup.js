e83ddda8a370f1e5273b657a4a372b72fa493842
phuproz56 <phuproz348@gmail.com>
2023-08-21 21:53:25 +0700

    update mongodb + token, sendemail, axios when sign up account

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,49 @@
+import nc from "next-connect";
+import db from "@/utils/db";
+import { validateEmail } from "@/utils/validation";
+import User from "@/models/User";
+import bcrypt from "bcrypt";
+import { createActivationToken } from "@/utils/tokens";
+import { sendEmail } from "@/utils/sendEmails";
+
+const handler = nc();
+
+handler.post(async (req, res) => {
+  try {
+    await db.connectDb();
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Vui lòng nhập đầy đủ." });
+    }
+    if (!validateEmail(email)) {
+      return res
+        .status(400)
+        .json({ message: "Email bạn nhập không đúng định dạng." });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email đã tồn tại." });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password phải ít nhất 6 kí tự." });
+    }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: cryptedPassword });
+    const addedUser = await newUser.save();
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
+    const url = `${process.env.BASE_URL}/activate/${activation_token}`;
+    sendEmail(email, url, "", "Activate your account.");
+    await db.disconnectDb();
+    res.json({
+      message: "Đăng ký thành công! vui lòng kính hoạt email để bắt đầu!",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+export default handler;
