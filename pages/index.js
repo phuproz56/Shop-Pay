b91511955d23b3f258e674decab6d40833e4c5b0
phuproz56 <phuproz348@gmail.com>
2023-09-02 16:03:13 +0700

    update css date: 2/9/2023

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,48 @@
-
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.scss';
-import Header from '@/components/header';
-import Footer from '@/components/footer';
-import axios from 'axios';
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.scss";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import axios from "axios";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Main from "@/components/home/main/index.js";
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home( { country } ) {
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home({ country }) {
   const { data: session } = useSession();
   // console.log(session);
   return (
-    <div>
+    <>
       <Header country={country} />
       {/* { session ? "you are logged in" : "you are not logged in" } */}
+      <div className={styles.home}>
+        <div className={styles.container}>
+          <Main />
+        </div>
+      </div>
       <Footer country={country} />
-
-      
-    </div>
-  )
+    </>
+  );
 }
 
 export async function getServerSideProps() {
-  let data = await axios.get('https://api.ipregistry.co/?key=whzhvdca3jwcascn').then((res) => {
-    return res.data.location.country;
-  }).catch((err) => {
-    console.log(err);
-  });
+  let data = await axios
+    .get("https://api.ipregistry.co/?key=whzhvdca3jwcascn")
+    .then((res) => {
+      return res.data.location.country;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   return {
     props: {
-      country: {name: data.name, flag: "https://png.pngtree.com/png-clipart/20200424/ourlarge/pngtree-round-creative-ink-brush-brush-vietnam-flag-png-image_2192072.jpg"},
-    }
-  }
+      country: {
+        name: data.name,
+        flag: "https://png.pngtree.com/png-clipart/20200424/ourlarge/pngtree-round-creative-ink-brush-brush-vietnam-flag-png-image_2192072.jpg",
+      },
+    },
+  };
 }
